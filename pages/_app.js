9cb30a5b4acbf9871715c654e56e30b06ec87af1
Nicolás Salas <nicolas.salas@imfd.cl>
2020-09-20 01:21:22 -0300

    Use emotion-reset and move globals to theme.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,23 +1,11 @@
 import { Global } from '@emotion/core'
 import { ThemeProvider } from 'emotion-theming'
-import { theme } from '../styles/theme'
-import { css } from '@emotion/core'
+import { theme, globalCss } from '../styles/theme'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
-      <Global
-        styles={css`
-          html,
-          body {
-            padding: 0;
-            margin: 0;
-            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-              sans-serif;
-          }
-        `}
-      />
+      <Global styles={globalCss} />
       <Component {...pageProps} />
     </ThemeProvider>
   )
--- styles/theme.js
+++ styles/theme.js
@@ -1,3 +1,6 @@
+import { css } from '@emotion/core'
+import emotionReset from 'emotion-reset'
+
 export const theme = {
   colors: {
     purple: '#7957d5',
@@ -5,4 +8,58 @@ export const theme = {
     purpleDark: '#6943d0',
     link: '#3273dc',
   },
+  typography: {
+    titleColor: '#363636',
+    subtitleColor: '#7a7a7a',
+    titleSize: '2rem',
+    subtitleSize: '1.75rem',
+  },
 }
+
+export const globalCss = (theme) => css`
+  ${emotionReset};
+
+  html,
+  body {
+    line-height: 1.2;
+    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
+      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
+  }
+
+  h1 {
+    color: ${theme.typography.titleColor};
+    font-size: ${theme.typography.titleSize};
+    padding: 0.5rem;
+  }
+
+  h2 {
+    color: ${theme.typography.subtitleColor};
+    font-size: ${theme.typography.subtitleSize};
+    padding: 0.5rem;
+  }
+
+  p {
+    padding: 0.5rem;
+  }
+
+  code {
+    font-family: monospace;
+  }
+
+  pre {
+    padding: 0.5rem;
+  }
+
+  ol {
+    list-style-type: decimal;
+  }
+
+  ul {
+    list-style-type: circle;
+  }
+
+  ol,
+  ul {
+    padding-left: 2.5rem;
+  }
+`
