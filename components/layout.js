4bf393372400b215a0f5cd564659df8db172dd4c
Nicolás Salas <nicolas.salas@imfd.cl>
2020-09-19 22:59:51 -0300

    Allow spacing the navbar

--- components/layout.js
+++ components/layout.js
@@ -1,11 +1,16 @@
 import { Container } from './container'
 import { Navbar } from '../components/navbar'
+import { css } from '@emotion/core'
 
-export const Layout = ({ children }) => {
+const spacedClass = css`
+  margin-top: 2rem;
+`
+
+export const Layout = ({ children, nospace = false }) => {
   return (
     <>
       <Navbar />
-      <Container>{children}</Container>
+      <Container css={nospace ? null : spacedClass}>{children}</Container>
     </>
   )
 }
