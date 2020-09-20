09e977bf66980a2c7a48c130da0880c72a981ef8
Nicolás Salas <nicolas.salas@imfd.cl>
2020-09-20 00:19:31 -0300

    Use emotion for global styles

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,23 @@
+import { Global } from '@emotion/core'
 import { ThemeProvider } from 'emotion-theming'
-import '../styles/globals.css'
 import { theme } from '../styles/theme'
+import { css } from '@emotion/core'
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider theme={theme}>
+      <Global
+        styles={css`
+          html,
+          body {
+            padding: 0;
+            margin: 0;
+            font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
+              Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
+              sans-serif;
+          }
+        `}
+      />
       <Component {...pageProps} />
     </ThemeProvider>
   )
